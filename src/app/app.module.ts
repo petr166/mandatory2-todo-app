a6a3a31d09813ce7bc9b1201006741371937b2ed
Petru Birzu <petru.birzu96@gmail.com>
2017-04-28 14:03:20 +0200

    created new-todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NewTodoComponent } from './components/new-todo/new-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTodoComponent
   ],
   imports: [
     BrowserModule,
